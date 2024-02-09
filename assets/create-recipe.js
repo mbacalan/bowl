0339462e2d743d89165d93e6f0d7a79bb6bf04ed
Muhammed Bacalan <muhammedbacalan@gmail.com>
2024-02-09 21:35:36 +0100

    feat: Add ability to remove ingredients and steps in Create Recipe form

--- assets/create-recipe.js
+++ assets/create-recipe.js
@@ -1,32 +1,50 @@
+/**
+  * @param {PointerEvent} event
+*/
+function removeInputGroup(event) {
+  const inputGroup = event.target.closest(".input-group")
+  if (inputGroup.parentElement.children.length <= 1) {
+    return
+  }
+
+  inputGroup.remove()
+}
+
 window.onload = () => {
   const ingredients = document.getElementById("ingredients")
   const addIngredientButton = document.getElementById("add-ingredient")
 
   addIngredientButton.addEventListener("click", () => {
     ingredients.insertAdjacentHTML("beforeend", `
-        <div class="input-group">
-          <input
-            type="text"
-            name="ingredient"
-            placeholder="Ingredient"
-            required
-          />
+      <div class="input-group">
+        <input
+          type="text"
+          name="ingredient"
+          placeholder="Ingredient"
+          required
+        />
 
-          <input
-            type="text"
-            name="quantity"
-            placeholder="Quantity"
-            required
-          />
+        <input
+          type="text"
+          name="quantity"
+          placeholder="Quantity"
+          required
+        />
 
-          <select
-            id="quantity-unit"
-            name="quantity-unit"
-            hx-get="/quantity-units"
-            hx-trigger="load"
-          />
-        </div>
+        <select
+          id="quantity-unit"
+          name="quantity-unit"
+          hx-get="/quantity-units"
+          hx-trigger="load"
+        ></select>
+
+        <span class="remove remove-ingredient">X</span>
+      </div>
     `)
+
+    document.querySelectorAll(".remove-ingredient").forEach(button => {
+      button.addEventListener("click", removeInputGroup)
+    })
 
     htmx.process(ingredients)
   })
@@ -43,9 +61,13 @@ window.onload = () => {
           placeholder="Step"
           required
         />
+
+        <span class="remove remove-step">X</span>
       </div>
     `)
 
-    htmx.process(steps)
+    document.querySelectorAll(".remove-step").forEach(button => {
+      button.addEventListener("click", removeInputGroup)
+    })
   })
 }
