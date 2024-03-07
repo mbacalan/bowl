08f09ca03837dc630b3900073504eacd83626eae
Muhammed Bacalan <muhammedbacalan@gmail.com>
2024-03-07 20:59:01 +0100

    chore: Minor template updates

--- components/pages/home_templ.go
+++ components/pages/home_templ.go
@@ -34,7 +34,7 @@ func Home(recipes []db.Recipe) templ.Component {
 				templ_7745c5c3_Buffer = templ.GetBuffer()
 				defer templ.ReleaseBuffer(templ_7745c5c3_Buffer)
 			}
-			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<h2>A database of your own recipes</h2><hr><h3><a href=\"/recipes\">Browse Recipes</a></h3><h3><a href=\"/ingredients\">Browse Ingredients</a></h3><h4>...or take a look at the recently created recipes</h4>")
+			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<h2>A database of your own recipes</h2><hr><h3><a href=\"/recipes\">🧾 Recipes</a></h3><h3><a href=\"/ingredients\">🍽 Ingredients</a></h3><h4>...or take a look at the recently created recipes</h4>")
 			if templ_7745c5c3_Err != nil {
 				return templ_7745c5c3_Err
 			}
