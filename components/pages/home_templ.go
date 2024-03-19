d1ce327b476538775ce9175facebc5071b3cba0b
Muhammed Bacalan <muhammedbacalan@gmail.com>
2024-03-19 22:04:17 +0100

    Style: Add emojis to homepage

--- components/pages/home_templ.go
+++ components/pages/home_templ.go
@@ -35,7 +35,7 @@ func Home(r []db.Recipe) templ.Component {
 				templ_7745c5c3_Buffer = templ.GetBuffer()
 				defer templ.ReleaseBuffer(templ_7745c5c3_Buffer)
 			}
-			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<h2>A database of your own recipes</h2><hr><h3><a href=\"/recipes\">🧾 Recipes</a></h3><h3><a href=\"/categories\">🍽 Categories</a></h3><h3><a href=\"/ingredients\">🍽 Ingredients</a></h3><h4>...or take a look at the recently created recipes</h4>")
+			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<h2>A database of your own recipes</h2><hr><h3><a href=\"/recipes\">📃 Recipes</a></h3><h3><a href=\"/categories\">📚 Categories</a></h3><h3><a href=\"/ingredients\">🍒 Ingredients</a></h3><h4>...or take a look at the recently created recipes</h4>")
 			if templ_7745c5c3_Err != nil {
 				return templ_7745c5c3_Err
 			}
