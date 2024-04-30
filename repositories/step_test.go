c5b8a54335af0008dfcb72435aaee91dfcd5a67e
Muhammed Bacalan <muhammedbacalan@gmail.com>
2024-04-30 22:11:52 +0200

    feat: Add tests for StepRepository

---
+++ repositories/step_test.go
@@ -0,0 +1,101 @@
+package repositories_test
+
+import (
+	"strconv"
+	"testing"
+
+	"github.com/mbacalan/bowl/repositories"
+)
+
+func mockStep(id uint) repositories.Step {
+	return repositories.Step{
+		RecipeID: id,
+		Step:     "Step " + strconv.Itoa(int(id)),
+	}
+}
+
+func TestStepRepository(t *testing.T) {
+	t.Run("get all", func(t *testing.T) {
+		db := setupTestDB(t)
+		repo := repositories.NewStepRepository(db, "steps")
+		expected := []repositories.Step{
+			mockStep(1),
+			mockStep(2),
+			mockStep(3),
+		}
+
+		db.Create(&expected)
+		actual, err := repo.GetAll()
+
+		if err != nil {
+			t.Errorf("error getting all steps: %v", err)
+		}
+
+		var steps []repositories.Step
+		var count int64
+		db.Find(&steps).Count(&count)
+
+		if count != int64(len(expected)) {
+			t.Errorf("expected %d steps, got %d", len(expected), count)
+		}
+
+		for i := range actual {
+			if expected[i].RecipeID != actual[i].RecipeID {
+				t.Errorf("expected recipe id %d, got %d", expected[i].RecipeID, actual[i].RecipeID)
+			}
+
+			if expected[i].Step != actual[i].Step {
+				t.Errorf("expected step %s, got %s", expected[i].Step, actual[i].Step)
+			}
+		}
+	})
+
+	t.Run("create", func(t *testing.T) {
+		db := setupTestDB(t)
+		repo := repositories.NewStepRepository(db, "steps")
+		expected := mockStep(1)
+
+		db.Create(&expected)
+		actual, err := repo.Create(expected.Step, expected.RecipeID)
+
+		if err != nil {
+			t.Errorf("error creating step: %v", err)
+		}
+
+		if expected.RecipeID != actual.RecipeID {
+			t.Errorf("expected recipe id %d, got %d", expected.RecipeID, actual.RecipeID)
+		}
+
+		if expected.Step != actual.Step {
+			t.Errorf("expected step %s, got %s", expected.Step, actual.Step)
+		}
+	})
+
+	t.Run("delete", func(t *testing.T) {
+		db := setupTestDB(t)
+		repo := repositories.NewStepRepository(db, "steps")
+		expected := []repositories.Step{
+			mockStep(1),
+			mockStep(2),
+			mockStep(3),
+		}
+
+		db.Create(&expected)
+
+		var steps []repositories.Step
+		db.Find(&steps)
+
+		err := repo.Delete(steps[0].ID)
+		if err != nil {
+			t.Errorf("error deleting steps: %v", err)
+		}
+
+		var count int64
+		db.Find(&steps).Count(&count)
+
+		expectedCount := int64(len(expected) - 1)
+		if count != expectedCount {
+			t.Errorf("expected %d steps, got %d", expectedCount, count)
+		}
+	})
+}
