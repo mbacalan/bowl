20cf07bd89832a68cc8ae8703a4f4dd81b7157fd
Muhammed Bacalan <muhammedbacalan@gmail.com>
2025-04-02 22:47:09 +0200

    style: Add Sakura dark theme support

--- components/partials/head_templ.go
+++ components/partials/head_templ.go
@@ -29,7 +29,7 @@ func Head() templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<head><meta charset=\"UTF-8\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"><title>🥗 Bowl</title><link rel=\"stylesheet\" href=\"/assets/modern-normalize.css\"><link rel=\"stylesheet\" href=\"/assets/sakura.css\"><link rel=\"stylesheet\" href=\"/assets/style.css\"><script src=\"/assets/htmx.min.js\"></script>")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<head><meta charset=\"UTF-8\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"><title>🥗 Bowl</title><link rel=\"stylesheet\" href=\"/assets/modern-normalize.css\"><link rel=\"stylesheet\" href=\"/assets/sakura.css\" media=\"screen\"><link rel=\"stylesheet\" href=\"/assets/sakura-dark.css\" media=\"screen and (prefers-color-scheme: dark)\"><link rel=\"stylesheet\" href=\"/assets/style.css\"><script src=\"/assets/htmx.min.js\"></script>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
