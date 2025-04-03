063df473d1d78c1cbecc735c95912b4792a1885b
Muhammed Bacalan <muhammedbacalan@gmail.com>
2025-04-03 17:43:41 +0200

    style: Add favicon's

--- components/partials/head_templ.go
+++ components/partials/head_templ.go
@@ -29,7 +29,7 @@ func Head() templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<head><meta charset=\"UTF-8\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"><title>🥗 Bowl</title><link rel=\"stylesheet\" href=\"/assets/modern-normalize.css\"><link rel=\"stylesheet\" href=\"/assets/sakura.css\" media=\"screen\"><link rel=\"stylesheet\" href=\"/assets/sakura-dark.css\" media=\"screen and (prefers-color-scheme: dark)\"><link rel=\"stylesheet\" href=\"/assets/style.css\"><script src=\"/assets/htmx.min.js\"></script>")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<head><meta charset=\"UTF-8\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"><title>🥗 Bowl</title><link rel=\"stylesheet\" href=\"/assets/modern-normalize.css\"><link rel=\"stylesheet\" href=\"/assets/sakura.css\" media=\"screen\"><link rel=\"stylesheet\" href=\"/assets/sakura-dark.css\" media=\"screen and (prefers-color-scheme: dark)\"><link rel=\"stylesheet\" href=\"/assets/style.css\"><link rel=\"apple-touch-icon\" sizes=\"180x180\" href=\"/assets/apple-touch-icon.png\"><link rel=\"icon\" type=\"image/png\" sizes=\"32x32\" href=\"/assets/favicon-32x32.png\"><link rel=\"icon\" type=\"image/png\" sizes=\"16x16\" href=\"/assets/favicon-16x16.png\"><link rel=\"manifest\" href=\"/assets/site.webmanifest\"><script src=\"/assets/htmx.min.js\"></script>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
