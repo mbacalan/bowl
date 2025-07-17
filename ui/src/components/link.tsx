209e4b60698af75e4b62d21dba0390012e4b3faa
Muhammed Bacalan <muhammedbacalan@gmail.com>
2025-07-17 14:26:35 +0200

    feat(ui): Add custom Link component for TS Router & Mantine

---
+++ ui/src/components/link.tsx
@@ -0,0 +1,19 @@
+import { Anchor, type AnchorProps } from "@mantine/core";
+import { createLink, type LinkComponent } from "@tanstack/react-router";
+import { forwardRef } from "react";
+
+interface MantineAnchorProps extends Omit<AnchorProps, "href"> {
+	// Add any additional props you want to pass to the anchor
+}
+
+const MantineLinkComponent = forwardRef<HTMLAnchorElement, MantineAnchorProps>(
+	(props, ref) => {
+		return <Anchor ref={ref} {...props} />;
+	},
+);
+
+const CreatedLinkComponent = createLink(MantineLinkComponent);
+
+export const Link: LinkComponent<typeof MantineLinkComponent> = (props) => {
+	return <CreatedLinkComponent preload="intent" {...props} />;
+};
